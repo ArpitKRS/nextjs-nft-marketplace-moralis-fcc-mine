a56530c6853cc2ccd965335f3ba615374e4afa4f
Arpit Kumar Singh <singharpit302@gmail.com>
2023-09-22 20:57:20 +0530

    meh, but still going

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
 import '../styles/globals.css'
 import { MoralisProvider } from 'react-moralis'
 import Header from '../components/Header'
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider initializeOnMount={false}>
-      <Header/>
-      <Component {...pageProps} />
-    </MoralisProvider>
+    <div>
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta name="description" content="NFT Marketplace"></meta>
+        <link rel="icon" href="/favicon.ico"></link>
+      </Head>
+      <MoralisProvider initializeOnMount={false}>
+        <Header/>
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (
         <div className={styles.container}>
-            <Head>
-                <title>NFT Marketplace</title>
-                <meta name="description" content="NFT Marketplace"></meta>
-                <link rel="icon" href="/favicon.ico"></link>
-            </Head>
+            
             Hi
         </div>
     )
